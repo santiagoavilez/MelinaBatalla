b2444605a65d0a26d87ebd265b88b1e15e0c78f4
santiagoavilez <santiagolingreed@gmail.com>
2023-05-25 21:25:18 -0300

    added: Accordion preguntas frecuentes y footer, astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()]
+  integrations: [tailwind(), solidJs(), image()]
 });
