fea66bee17c1b894c77834db8a18ca6ac385c552
santiagoavilez <santiagolingreed@gmail.com>
2023-10-02 18:28:06 -0300

    deleted identidad-visual

--- src/components/ui/Accordion.tsx
+++ src/components/ui/Accordion.tsx
@@ -1,4 +1,4 @@
-/** @jsxImportSource solid-js */
+
 import { createSignal } from 'solid-js';
 import { Collapse } from 'solid-collapse';
 
@@ -16,10 +16,10 @@ export default function Accordion({ title, content }: AccordionProps) {
         <section class='my-1.5'>
             <header class={`${isExpanded() ? 'bg-negro text-white transition-all' : ''}  border border-negro/50 w-full p-2 `}>
                 <button class={`  w-full flex justify-between items-center `} onClick={handleExpand}>
-                    <span class='text-left'> {title}</span>
+                    <span class='text-left'>{title}</span>
                     <svg id="Capa_2" class={` ${isExpanded() ? 'stroke-white ' : 'stroke-negro rotate-90  fill-white '}  transition-all  w-5 `} data-name="Capa 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 49.48 49.71">
 
-                        <g style="fill: none;stroke-miterlimit: 10; stroke-width: 2px;" id="Mobil">
+                        <g style={{ fill: none, strokeMiterLimit: 10, strokeWidth: "2px" }} id="Mobil">
                             <g id="S7">
                                 <g>
                                     <line class="cls-1" x1=".71" y1="49" x2="48.48" y2="1.22" />
@@ -37,6 +37,6 @@ export default function Accordion({ title, content }: AccordionProps) {
                 </p>
             </Collapse>
 
-        </section>
+        </section >
     );
 };
