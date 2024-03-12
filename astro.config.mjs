7b80fb664201312d5cd530615926e4995ce4fb65
santiagoavilez <santiagolingreed@gmail.com>
2024-03-12 12:41:20 -0300

    fix: adapter vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import solid from "@astrojs/solid-js";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-
   output: 'server',
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react({
-      experimentalReactChildren: true,
-
-    }),
-  ]
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react({
+    experimentalReactChildren: true
+  })],
+  adapter: vercel()
 });
